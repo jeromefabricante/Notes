cabe80456e9ceeddef9479d735d210cac0f1ee0d
jerome <jerome@wizmediateam.com>
2012-07-06 16:22:36 +0800

    Messages

---
+++ phptest/arrays.php
@@ -0,0 +1,11 @@
+<?php
+	$array = array( "var"=>"Nice", "var2"=>"Try");
+	
+	$array['value'] = 1;
+	echo print_r($array);
+
+	$array2 = array("Nice", "try");
+
+	echo "<br/>" . print_r($array2);
+
+?>
